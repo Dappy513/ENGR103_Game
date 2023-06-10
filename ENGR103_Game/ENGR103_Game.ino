95b9ad1d714f33741b4f27dbbb7dcad392ed63da
Dappy513 <135365137+Dappy513@users.noreply.github.com>
2023-06-10 14:29:40 -0700

    6/10 ENGR103_Game.ino
    Made level beeps that increase in tone based on difficulty.
    Added interrupt into the for loop to allow me to record the current state of the for loop.
    Debugged many issues in my rough drafts.

--- ENGR103_Game/ENGR103_Game.ino
+++ ENGR103_Game/ENGR103_Game.ino
@@ -1,87 +1,116 @@
 #include <Adafruit_CircuitPlayground.h>
 
-volatile int modeControl = 0;
-volatile int flip = 0;
+const int initSpeed = 250; //initial speed that the colors rotate in ms per LED
+const int initLevel = 1; //starting level
+const int levelScale = 10; //amount that the speed increases by in ms
+volatile int levelNum = initLevel; //indicator of current level
+volatile int LEDspeed = initSpeed; //current level speed
+
 const int buttonA = 4;
 const int buttonB = 5;
 const int switchPin = 7;
+volatile int k = 0;
+const int notes[] = {262,294,330,349,392,440,494,523,650,750,850,950,1050,1150,1250,1350,1450};
 
 bool switchState = 0;
 volatile bool A_flag = false;
 volatile bool B_flag = false;
 volatile bool switchFlag = false;
+volatile bool stopFlag = 0;
+volatile bool resetFlag = 0;
+volatile bool gameOver = 0;
+volatile bool pauseFlag = 0;
+
+int something;
 
 void setup() 
 {
   CircuitPlayground.begin();
-  randomSeed(analogRead(0)); // Seed the random, randomly
-  attachInterrupt(digitalPinToInterrupt(buttonA), stop, RISING);
+  attachInterrupt(digitalPinToInterrupt(buttonA), pause, RISING);
   attachInterrupt(digitalPinToInterrupt(buttonB), reset, RISING);
   attachInterrupt(digitalPinToInterrupt(switchPin), on_off, CHANGE);
+  switchState = digitalRead(switchPin);
 }
 
 void loop() {
-  if(switchFlag)
+  CircuitPlayground.setPixelColor(7, 255, 0, 0);
+  if(switchFlag)     //switch turns the game on or off
   {
+    Serial.println("Interrupt");
     delay(5);
     switchState = digitalRead(switchPin);
-    switchFlag = !switchFlag;
+    Serial.println(switchState);
+    switchFlag = 0;
   }
-  if(switchState)
+  if(switchState == 1)
   {
+    for(int k = 0; k < 10; k++)
+    {
+      int pin_num = k;
+      int pin_two = k+1;
+      int pin_three = k-1;
+      if(gameOver == 1)
+      {
+        CircuitPlayground.playTone(400, 300);
+        if(resetFlag)
+        {
+          CircuitPlayground.playTone(1400, 150);
+          levelNum = initLevel;
+          LEDspeed = initSpeed;
+          gameOver = 0;
+          resetFlag = !resetFlag;
+        }
+      }
+      CircuitPlayground.setPixelColor(pin_num, 0, 0, 255);
+      CircuitPlayground.setPixelColor(pin_two, 0, 0, 255);
+      CircuitPlayground.setPixelColor(pin_three, 0, 0, 255);
+      delay(LEDspeed);
+      CircuitPlayground.setPixelColor(pin_num, 0, 0, 0);
+      CircuitPlayground.setPixelColor(pin_two, 0, 0, 0);
+      CircuitPlayground.setPixelColor(pin_three, 0, 0, 0);  
 
+      if (pauseFlag == 1){
+        pauseFlag = 0;
+        stop(k);
+      }
+    pauseFlag = 0;
+    }
   }
-
-
+  else if(switchState == 0);
+  {
+    for(int i = 0; i < 10; i++)
+    {
+      CircuitPlayground.setPixelColor(i, 0, 0, 0);
+    }
+  }
 }
 
-void bounce(int flashTime)
-{
-  for(int k = 0; k < 5; k++)
-  {
-    int pin_num = k;
-    int pin_two = 9-k;
-    CircuitPlayground.setPixelColor(pin_num, 0, 0, 255);
-    CircuitPlayground.setPixelColor(pin_two, 255, 0, 0);
-    delay(flashTime/2);
-    CircuitPlayground.setPixelColor(pin_num, 0, 0, 0);
-    CircuitPlayground.setPixelColor(pin_two, 0, 0, 0);
-    delay(flashTime/2);
-    k++;
-  }
-    for(int k = 5; k > 0; k--)
-  {
-    int pin_num = k;
-    int pin_two = 9-k;
-    CircuitPlayground.setPixelColor(pin_num, 0, 0, 255);
-    CircuitPlayground.setPixelColor(pin_two, 255, 0, 0);
-    delay(flashTime/2);
-    CircuitPlayground.setPixelColor(pin_num, 0, 0, 0);
-    CircuitPlayground.setPixelColor(pin_two, 0, 0, 0);
-    delay(flashTime/2);
-    k--;
-  }
+void pause(){
+  pauseFlag = 1;
 }
-
-void stop()
+void stop( int k)
 {
-  modeControl++;
-  if(modeControl > 4)
+  pauseFlag = 0;
+  int LEDspot = k;
+  Serial.println(k);
+  if(LEDspot == 6 || LEDspot == 7 || LEDspot == 8)
   {
-    modeControl = 0;
+    CircuitPlayground.playTone(notes[levelNum], 500);
+    LEDspeed = 0.8*LEDspeed + 1;
+    levelNum++;
+  }
+  else
+  {
+    gameOver = 1;
   }
 }
 
 void reset()
 {
-  modeControl--;
-  if(modeControl < 0)
-  {
-    modeControl = 4;
-  }
+  resetFlag = !resetFlag;
 }
 
 void on_off()
 {
-  switchFlag = !switchFlag;
+  switchFlag = 1;
 }
